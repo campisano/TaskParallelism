cbf264cd9c205edb155f49a9e2c30e5bb159252d
campisano <campisano@lineacln14>
2016-06-08 13:32:35 -0300

    DFSHadoopLIneA minor refactoring

--- task_parallelism/dfs/DFSHadoopLIneA.py
+++ task_parallelism/dfs/DFSHadoopLIneA.py
@@ -12,6 +12,7 @@ class DFSHadoopLIneAFactory(DFSHadoopFactory):
     def __init__(
         self
     ):
+        ####
         # TODO [CMP] gambi!
         if "PATH" not in os.environ:
             os.environ["PATH"] = "/bin:/usr/bin"
@@ -20,7 +21,9 @@ class DFSHadoopLIneAFactory(DFSHadoopFactory):
         os.environ["HOME"] = "/home/riccardo.campisano"
         os.environ["JAVA_HOME"] = "/tawala/packages/jdk-1.7u25-1"
 
-        if os.path.isdir("/home/riccardo.campisano/hadoop-install/hadoop-2.6.0"):
+        if os.path.isdir(
+            "/home/riccardo.campisano/hadoop-install/hadoop-2.6.0"
+        ):
             os.environ["HADOOP_HOME"] = (
                 "/home/riccardo.campisano/hadoop-install/hadoop-2.6.0")
 
@@ -28,20 +31,7 @@ class DFSHadoopLIneAFactory(DFSHadoopFactory):
             os.environ["HADOOP_HOME"] = (
                 "/hadoop-install/hadoop-2.6.0")
 
-        ####
-        # Defining environment
-        if not os.getenv("USER"):
-            raise Exception("Can not find env variable 'USER'.")
-
-        self.user = os.getenv("USER")
-
-        self.hadoop_home = os.getenv("HADOOP_HOME")
-
-        ####
-        # Define initial vars
-        self.hadoop_user_base_path = os.path.join("/user", self.user)
-        self.hadoop_cmd = os.path.join(self.hadoop_home, "bin", "hadoop")
-        self.hadoop_blocksize = 4194304     # 4MB
+        DFSHadoopFactory.__init__(self)
 
     def create(
         self
@@ -60,6 +50,7 @@ class DFSHadoopLIneA(DFSHadoop):
         _hadoop_cmd,
         _hadoop_blocksize
     ):
+        ####
         # TODO [CMP] gambi!
         if "PATH" not in os.environ:
             os.environ["PATH"] = "/bin:/usr/bin"
@@ -68,7 +59,9 @@ class DFSHadoopLIneA(DFSHadoop):
         os.environ["HOME"] = "/home/riccardo.campisano"
         os.environ["JAVA_HOME"] = "/tawala/packages/jdk-1.7u25-1"
 
-        if os.path.isdir("/home/riccardo.campisano/hadoop-install/hadoop-2.6.0"):
+        if os.path.isdir(
+            "/home/riccardo.campisano/hadoop-install/hadoop-2.6.0"
+        ):
             os.environ["HADOOP_HOME"] = (
                 "/home/riccardo.campisano/hadoop-install/hadoop-2.6.0")
 
